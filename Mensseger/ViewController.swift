f78564206f50f9172d5c4b67895fe3c3813239cf
Ismael Lima <79029743+L3EWick@users.noreply.github.com>
2023-09-27 14:07:04 -0300

    Initial Commit

---
+++ Mensseger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mensseger
+//
+//  Created by Ismael Lima on 27/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
